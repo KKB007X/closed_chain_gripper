d8f3139e6ae145effb3380663501db96db0f62b7
KKB007X <kamaleshkumarb07@gmail.com>
2026-06-07 16:22:03 +0530

    Delete build directory

--- build/gripper_plugins/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kkb007x/closed_chain_gripper/src/gripper_plugins
-# Build directory: /home/kkb007x/closed_chain_gripper/build/gripper_plugins
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/gripper_plugins/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/kkb007x/closed_chain_gripper/build/gripper_plugins/libgripper_plugin.so" "TARGETS" "gripper_plugin" "LIBRARY_DESTINATION" "lib")
